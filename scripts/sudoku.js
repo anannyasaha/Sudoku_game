8a457382f10744d662835ed019b769b71e6f435d
root <root@DESKTOP-DF8L8DR.localdomain>
2021-02-17 23:59:06 +0600

    lab1 and lab2 done working on lab3

--- scripts/sudoku.js
+++ scripts/sudoku.js
@@ -1,1 +1,32 @@
-// add your code here
+window.onload=function(){
+    
+        let Board=document.getElementById('Board');
+        let tablerow=document.getElementById('thickBorder');
+        let table=document.getElementById('TABLE');
+    for (let p=0;p<3;p++){
+       for(let k=0;k<3;k++){
+        for(let i=0;i<3;i++){
+         let tr=document.createElement("tr");
+         for(let j=0;j<3;j++){
+             let td=document.createElement("td");
+             td.setAttribute('id','content');
+             tr.append(td);
+         }
+         table.append(tr);
+        }
+        tablerow.append(table);
+    }
+    Board.append(tablerow);
+    }
+};
+    let palette=document.getElementById('Pallete');
+    let td=document.createElement("td");
+    for (let i=0;i<10;i++){
+        let td=document.createElement("td");
+        td.click(function(){
+            console.log("click");
+        })
+    }
+    
+    
+
